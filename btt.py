c3a2a08b39e63dd385d182ce517c9c150325e43e
datsun240z <rbelaire@gmail.com>
2014-06-01 22:05:24 -0400

    Modify btt to file finished bts that are finished and are in rar format.

--- btt.py
+++ btt.py
@@ -1,37 +1,52 @@
 import os.path
 
-torrent = {'files': [
+torrents = [{'files': [
                      {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/Sample/family.guy.s12e15.hdtv.x264-excellence.sample.mp4', 'length': 3538862, 'bytesCompleted': 3538862},
                      {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.nfo', 'length': 1048, 'bytesCompleted': 1048},
                      {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r01', 'length': 15000000, 'bytesCompleted': 15000000},
                      {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r02', 'length': 15000000, 'bytesCompleted': 15000000},
                      {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r00', 'length': 15000000, 'bytesCompleted':15000000},
                      {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r03', 'length': 8917956, 'bytesCompleted': 8917956},
-                     {'name':'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.rar', 'length': 15000000, 'bytesCompleted': 15000000},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.rar', 'length': 15000000, 'bytesCompleted': 15000000},
                      {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.sfv', 'length': 265, 'bytesCompleted': 265}
                     ],
            'id': 94,
-           'isFinished': False} 
+           'isFinished': False},
+          {'files': [
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/Sample/family.guy.s12e15.hdtv.x264-excellence.sample.mp4', 'length': 3538862, 'bytesCompleted': 3538862},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.nfo', 'length': 1048, 'bytesCompleted': 1048},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r01', 'length': 15000000, 'bytesCompleted': 15000000},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r02', 'length': 15000000, 'bytesCompleted': 15000000},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r00', 'length': 15000000, 'bytesCompleted':15000000},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.r03', 'length': 8917956, 'bytesCompleted': 8917956},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.rar', 'length': 15000000, 'bytesCompleted': 15000000},
+                     {'name': 'Family.Guy.S12E15.HDTV.x264-EXCELLENCE/family.guy.s12e15.hdtv.x264-excellence.sfv', 'length': 265, 'bytesCompleted': 265}
+                    ],
+           'id': 94,
+           'isFinished': True} ]
 
-def is_rar(torrent):
-    return True
+for t in torrents:
+    if t.get('isFinished', False):
+        for f in t.get('files', []):
+            if f['name'].endswith('.rar'):
+                print (os.path.basename(f['name']))
 
-def is_finished_and_in_rar(torrent):
-    #[k for k, v in torrent['files'].items() if v < 1500000]
-    return not print (torrent['files'])
-    
-print(torrent)
-print("Value : %s" %  torrent.get('id'))
-print("Value : %s" %  torrent.get('isFinished'))
+#(any([ f['name'].endswith('.rar')
+        #for f in torrent.get('files', [])])
+#and
+#all([ f['length'] == f['bytesCompleted']
+        #for f in torrent.get('files', [])]))
 
 #print("Value : %s" %     torrent.get('files'))
-new_items = [i for i in  torrent.get('files')
-                 if i["name"] ]
-item_names = [name['name'] for name in new_items]
+#new_items = [i for i in torrent['files'] if i["name"] ]
+#new_items = [i for i in torrent['files'] if "name" in 
+#item_names = [name['name'] for name in new_items]
 
-for sn in item_names:
-    print(os.path.basename(sn))
+#new_items = [ file["name"] for file in torrent["files"]]
+#item_names = [ file["name"] for file in torrent["files"] if file["name"]]
+
+#for sn in item_names:
+    #print(os.path.basename(sn))
 
 #for k, v in torrent.items() if k == "id":
     #print "%s=%s" % (k, v)
-
