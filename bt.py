d40d5ebf9abaf5061339ca314b8c00bfbfb2a2d3
datsun240z <rbelaire@gmail.com>
2014-06-01 22:36:59 -0400

    Now it will ls the finished rar files.

--- bt.py
+++ bt.py
@@ -1,26 +1,22 @@
+import os.path
 from transmission import Transmission
 # pip install transmission-fluid
 
 client = Transmission(host='192.168.0.111', port=9099, username='admin', password='password')
 
 session = client('session-get')
-print (session['rpc-version'])
-print (session['rpc-version-minimum'])
-print (session['version'])
+#print (session['rpc-version'])
+#print (session['rpc-version-minimum'])
+#print (session['version'])
 
-def is_rar(torrent):
-    return True
-
-def is_finished_and_in_rar(torrent):
-    #[k for k, v in torrent['files'].items() if v < 1500000]
-    return not print (torrent['files'])
-
-    
 # When no ids are given, grabs from all torrents
-torrents = client('torrent-get', ids=94, fields=['id', 'isFinished', 'files'])['torrents']
-print(torrents)
-torrent_ids = [torrent['id'] for torrent in torrents if is_finished_and_in_rar(torrent)]
-print(torrent_ids)
+#torrents = client('torrent-get', ids=94, fields=['id', 'isFinished', 'files'])['torrents']
+torrents = client('torrent-get', fields=['id', 'isFinished', 'files'])['torrents']
+#torrent_ids = [torrent['id'] for torrent in torrents if is_finished_and_in_rar(torrent)]
 
-#client('torrent-remove', ids=torrent_ids)
-#files = [torrent['files'] for torrent in torrents if is_rar(torrent)]
+for t in torrents:
+    if t.get('isFinished', False):
+        for f in t.get('files', []):
+            if f['name'].endswith('.rar'):
+                print (os.path.basename(f['name']))
+
