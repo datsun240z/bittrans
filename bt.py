2bdd5700d92d679e300daf2051c10c74ac231414
datsun240z <rbelaire@gmail.com>
2014-06-03 20:38:26 -0400

    Now removes the torrent when done

--- bt.py
+++ bt.py
@@ -1,6 +1,5 @@
 import os.path
 import sys
-import rarfile
 from transmission import Transmission
 # pip install transmission-fluid
 from subprocess import call
@@ -13,21 +12,17 @@ session = client('session-get')
 #torrents = client('torrent-get', ids=94, fields=['id', 'isFinished', 'files'])['torrents']
 torrents = client('torrent-get', fields=['id', 'isFinished', 'files'])['torrents']
 #torrent_ids = [torrent['id'] for torrent in torrents if is_finished_and_in_rar(torrent)]
-rarfile.UNRAR_TOOL = "/c/Program\ Files\ \(x86\)/Unrar/UnRAR.exe"
+#rarfile.UNRAR_TOOL = "/c/Program\ Files\ \(x86\)/Unrar/UnRAR.exe"
+torrent_ids = []
 
 for t in torrents:
     if t.get('isFinished', False):
         for f in t.get('files', []):
             if f['name'].endswith('.rar'):
                 fn = f['name']
-                #fn = fn.replace("\\", "/")
-                #fn = os.path.join('/m/Videos/torrents/', fn)
+                #call(["UnRAR", "lb", fn])
                 call(["UnRAR", "x", fn])
-                print(fn)
-                #rf = rarfile.RarFile(fn)
-                #for fn in rf.infolist():
-                #    print (fn.filename, fn.file_size)
-                #    if fn.filename == 'README':
-                #        print(rf.read(fn))
+                torrent_ids.append(t.get('id', []))
 
+client('torrent-remove', ids=torrent_ids)
 sys.exit(1)
